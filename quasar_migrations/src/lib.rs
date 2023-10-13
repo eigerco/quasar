7d4a600789810cd7aaa040b99d006c6fbfd3d64a
Geoffrey Mureithi <mureithinjuguna@gmail.com>
2023-10-13 10:12:49 +0300

    add: create contract table migration

--- quasar_migrations/src/lib.rs
+++ quasar_migrations/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_ledgers;
+mod m20231013_065827_create_contract_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_ledgers::Migration)]
+        vec![
+            Box::new(m20220101_000001_create_ledgers::Migration),
+            Box::new(m20231013_065827_create_contract_table::Migration),
+        ]
     }
 }
---
+++ quasar_migrations/src/m20231013_065827_create_contract_table.rs
@@ -0,0 +1,56 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Contract::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Contract::Address)
+                            .string()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(Contract::Hash)
+                            .string()
+                            .not_null()
+                            .unique_key(),
+                    )
+                    .col(ColumnDef::new(Contract::Key).string().not_null())
+                    .col(ColumnDef::new(Contract::Type).string().not_null())
+                    .col(
+                        ColumnDef::new(Contract::LastModified)
+                            .date_time()
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Contract::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Contract {
+    #[sea_orm(iden = "contracts")]
+    Table,
+    Address,
+    Hash,
+    Key,
+    Type,
+    LastModified,
+    // TODO: @geofmureithi Soroban contracts can be introspected.
+    // Methods
+}
