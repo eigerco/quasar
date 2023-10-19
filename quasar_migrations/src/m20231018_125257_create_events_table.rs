64b589e4a22d000a0f22f50fff799a2079ff9366
Geoffrey Mureithi <mureithinjuguna@gmail.com>
2023-10-19 15:58:26 +0300

    add: events table

---
+++ quasar_migrations/src/m20231018_125257_create_events_table.rs
@@ -0,0 +1,47 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Event::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Event::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Event::Topic).string().not_null())
+                    .col(ColumnDef::new(Event::ContractId).string().not_null())
+                    .col(ColumnDef::new(Event::Ledger).integer().not_null())
+                    .col(ColumnDef::new(Event::Value).json().not_null())
+                    .col(ColumnDef::new(Event::Type).string().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Event::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Event {
+    Table,
+    Id,
+    Topic,
+    ContractId,
+    Ledger,
+    Value,
+    Type
+}
