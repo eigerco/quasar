eafd9ceabf4bc5381d3ba028e8ddd4d15873638f
Geoffrey Mureithi <95377562+geofmureithi@users.noreply.github.com>
2023-11-06 10:52:05 +0300

    fix: prevent on conflict error (#63)
    * fix: prevent on conflict error
    
    * fix: update some columns
    
    * fix: include all account columns
    
    * fix: include all contract columns

--- src/ingestion/accounts.rs
+++ src/ingestion/accounts.rs
@@ -2,8 +2,9 @@ use crate::databases::{NodeDatabase, QuasarDatabase};
 
 use super::{IngestionError, IngestionMetrics};
 use log::info;
+use migration::OnConflict;
 use quasar_entities::account;
-use sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, QueryFilter};
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 use stellar_node_entities::{accounts, prelude::Accounts};
 
 pub(super) async fn ingest_accounts(
@@ -38,7 +39,23 @@ pub(super) async fn ingest_account(
 ) -> Result<(), IngestionError> {
     let account: account::ActiveModel = account::ActiveModel::try_from(account)?;
 
-    account.insert(db.as_inner()).await?;
-
+    account::Entity::insert(account)
+        .on_conflict(
+            OnConflict::column(account::Column::Id)
+                .update_columns([
+                    account::Column::LastModified,
+                    account::Column::Balance,
+                    account::Column::BuyingLiabilities,
+                    account::Column::HomeDomain,
+                    account::Column::InflationDestination,
+                    account::Column::MasterWeight,
+                    account::Column::NumberOfSubentries,
+                    account::Column::SellingLiabilities,
+                    account::Column::SequenceNumber,
+                ])
+                .to_owned(),
+        )
+        .exec(db.as_inner())
+        .await?;
     Ok(())
 }
